f89177e8962d2b78476ac78e76caf4b7d079005b
terrysducks <41840392+terrysducks@users.noreply.github.com>
2019-04-08 19:12:39 -0400

    try 100

--- server.js
+++ server.js
@@ -58,13 +58,13 @@ app.use('/graphql', bodyParser.json(),
     }))
 )
 
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static("client/build"));
-  
-    app.get("*", (req, res) => {
-      res.sendFile(path.join(__dirname, "client/build", "index.html"));
-    });
-  }
+if(process.env.NODE_ENV === 'production'){
+    //set static folder
+    app.use(express.static('client/build'));
+}
+app.get('*',(req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
 
 
 const PORT = process.env.PORT || 4444;
